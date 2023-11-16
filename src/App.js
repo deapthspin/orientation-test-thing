ca4aeab509bd5b8121e69de7fa77678b9ca2308b
deapthspin <thedeapthspin@gmail.com>
2023-11-16 13:20:46 +0800

    tetffdgd

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   const [alpha, setAlpha] = useState(0)
   const [beta, setBeta] = useState(0)
   const [gamma, setGamma] = useState(0)
+  const [message, setMessage] = useState('')
 
   const handleClick = () => {
     if (typeof DeviceMotionEvent.requestPermission === 'function') {
@@ -25,21 +26,24 @@ function App() {
     }
   }
 
+  window.addEventListener('deviceorientation', handleOrientation);
+
   function handleOrientation(event) {
     const a = event.alpha;
     const b = event.beta;
     const g = event.gamma;
-    
+    console.log(event.alpha)
     setAlpha(a)
     setBeta(b)
     setGamma(g)
   }
   return (
     <div className="App">
-      <button onClick={handleClick}>enable</button>
+      {/* <button onClick={handleClick}>enable</button> */}
       <h1>alpha: {alpha}</h1>
       <h1>beta: {beta}</h1>
       <h1>gamma: {gamma}</h1>
+      <h1>{message}</h1>
     </div>
   );
 }
