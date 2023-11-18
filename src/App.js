b8307b3e49ca480666a12c758f7d78362aa564e1
deapthspin <thedeapthspin@gmail.com>
2023-11-18 14:36:56 +0800

    asd

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ function App() {
   const [alpha, setAlpha] = useState(0)
   const [beta, setBeta] = useState(0)
   const [gamma, setGamma] = useState(0)
-  const [message, setMessage] = useState('')
-
+  const [chosenImage, setChosenImage] = useState('')
   const handleClick = () => {
     if (typeof DeviceMotionEvent.requestPermission === 'function') {
       // Handle iOS 13+ devices.
@@ -28,29 +27,45 @@ function App() {
     }
   }
 
-
+  function chooseimage() {
+    let folder = require.context('./images', true)
+    let imageList = folder.keys().map(image => folder(image));
+    let random = Math.floor(Math.random() * imageList.length)
+    let chosen = imageList[0]
+    // console.log(chosen)
+    setChosenImage(chosen)
+  }
 
   function handleOrientation(event) {
     const a = event.alpha;
     const b = event.beta;
     const g = event.gamma;
-    console.log(event.alpha)
     setAlpha(Math.round(a))
     setBeta(Math.round(b))
     setGamma(Math.round(g))
   }
+  useEffect(() => {
+    if(gamma < 15 && gamma > -15 && (beta > 170 || beta < -170)) {
+      chooseimage()
+    }
+    
+  }, [beta, gamma])
   return (
     <div className="App">
       <button onClick={handleClick}>ensable</button>
       <h2>alpha: {Math.round(alpha)}</h2>
       <h2>beta: {Math.round(beta)}</h2>
       <h2>gamma: {Math.round(gamma)}</h2>
-      {beta < 3 && beta >= -1 && gamma < 3 && gamma >= -1 && <h1>lying down</h1>}
-      {beta < 95 && beta > 80 && <h1>upright</h1>}
-      {gamma < 15 && gamma > -15 && (beta > 170 || beta < -170) && <h1>facing down</h1>}
-      {beta <= 1 && beta >= -1 && (gamma <= -80 && gamma >= -90 || gamma <= 90 && gamma >= 80) && <h1>landscape</h1>}
-      
-      <h1>{message}</h1>
+      {/* {} */}
+      {/* {beta < 3 && beta >= -1 && gamma < 3 && gamma >= -1 && <h1>lying down</h1>}
+      {beta < 95 && beta > 80 && <h1>upright</h1>} */}
+      {beta <= 1 && beta >= -1 && (gamma <= -80 && gamma >= -90 || gamma <= 90 && gamma >= 80) && <img className='randImg' src={chosenImage}/>}
+      {gamma < 15 && gamma > -15 && (beta > 170 || beta < -170) && <div>
+        
+        <h1>picking animals</h1>
+      </div>}
+     
+
     </div>
   );
 }
