96c12e906852236fb3f124ccde3a7694ed20b70a
deapthspin <thedeapthspin@gmail.com>
2023-11-19 12:54:28 +0800

    fsasdfsdfdsfs

--- src/App.js
+++ src/App.js
@@ -53,9 +53,9 @@ function App() {
   return (
     <div className="App">
       <button onClick={handleClick}>ensable</button>
-      {/* <h2>alpha: {Math.round(alpha)}</h2>
+      <h2>alpha: {Math.round(alpha)}</h2>
       <h2>beta: {Math.round(beta)}</h2>
-      <h2>gamma: {Math.round(gamma)}</h2> */}
+      <h2>gamma: {Math.round(gamma)}</h2>
       {/* {} */}
       {/* {beta < 3 && beta >= -1 && gamma < 3 && gamma >= -1 && <h1>lying down</h1>}
       {beta < 95 && beta > 80 && <h1>upright</h1>} */}
