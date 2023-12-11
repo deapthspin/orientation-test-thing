a7e3877cffc386d4739824a86726e7b91e4ca5ed
deapthspin <thedeapthspin@gmail.com>
2023-12-11 13:12:36 +0800

    asdsdfsd

--- src/components/joinroomfromqr.js
+++ src/components/joinroomfromqr.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 function Joinroomfromqr() {
     const roomId = useParams().id
     const [name, setName] = useState('')
-
+    const navigate = useNavigate()
     function nameChange(e) {
         
             setName(e.target.value)
@@ -14,21 +14,21 @@ function Joinroomfromqr() {
     async function joinGame(e) {
         e.preventDefault()
         if(name.trim()) {
-            const res1 = await fetch(`https://animalguessingpg.onrender.com/playersinroom/${inputId}`)
+            const res1 = await fetch(`https://animalguessingpg.onrender.com/playersinroom/${roomId}`)
             const data1 = await res1.json()
     
             // console.log(data1.players)
             if(!data1.players.includes(name)) {
             
                     console.log('sent')
-                    const res = await fetch(`https://animalguessingpg.onrender.com/rooms/${inputId}`)
+                    const res = await fetch(`https://animalguessingpg.onrender.com/rooms/${roomId}`)
                     const data = await res.json()
         
                     let tempplayerArr = [...data.players]
                     
                    tempplayerArr.push(name)
                     console.log(tempplayerArr.toString())
-                    await fetch(`https://animalguessingpg.onrender.com/roomplayers/${inputId}`, {
+                    await fetch(`https://animalguessingpg.onrender.com/roomplayers/${roomId}`, {
                         method: "PATCH",
                         headers: {
                             'Content-Type': 'application/json'
@@ -38,13 +38,13 @@ function Joinroomfromqr() {
                         })
                     })
         
-                    ws.current.send(
-                        JSON.stringify({
-                            roomId: roomId,
-                            username: name,
-                            msgType: 'joinroom'
-                        })
-                    )
+                    // ws.current.send(
+                    //     JSON.stringify({
+                    //         roomId: roomId,
+                    //         username: name,
+                    //         msgType: 'joinroom'
+                    //     })
+                    // )
                     
                     navigate(`/room/${roomId}`)
                 
