e06d3d56062eda8332907e35a1930a373a4b63af
deapthspin <thedeapthspin@gmail.com>
2023-12-16 15:05:56 +0800

    ffdbnbv

--- src/components/guessing.js
+++ src/components/guessing.js
@@ -312,6 +312,7 @@ function Guessing(props) {
           <h1>score: {score}</h1>
           <h1>you got {(score / 10) * 100}% of questions correct</h1>
         </div>}
+        {console.log(plrScores)}
         {finished && isowner === 'yes' && plrScores.length > 0 && <div>
           <ol>
             {plrScores.map((plr) => {
