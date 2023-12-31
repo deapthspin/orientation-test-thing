5957d74f01d5a6fc0405014465d58faf17aaa57a
deapthspin <thedeapthspin@gmail.com>
2023-12-31 20:03:57 +0800

    sdfsdfsdfsdf

--- src/components/guessing.js
+++ src/components/guessing.js
@@ -172,12 +172,13 @@ function Guessing(props) {
                 setIsVoting(isVoting = false)
                 let num = 0
                 let highest = {}
+                console.log(item, playerVotes, playerlist, plrScores)
                 playerVotes.map((item) => {
                   if(item.score > num) {
                     num = item.score
                     highest = item
                     setHighestVotes(item)
-                    console.log(item, playerVotes)
+                    
                   }
                 })
                 
