1bc2281dd550c4fab477d3a01ae35872ead195be
deapthspin <thedeapthspin@gmail.com>
2024-01-03 15:17:32 +0800

    adsa

--- src/components/guessing.js
+++ src/components/guessing.js
@@ -375,6 +375,7 @@ function Guessing(props) {
       }))
       setAns(['', ''])
       setCardschosen([{}, {}])
+      setPlayerans([])
       if(!qnComplete) {
         setQnComplete(true)
         ws.current.send(JSON.stringify({
